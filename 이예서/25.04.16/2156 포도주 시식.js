867269c105de7b261261bcd4152f2a5c6b8941a2
Lee Ye Seo <rosefor2ne@gmail.com>
2025-04-16 17:56:46 +0900

    fix: 포도주 시식 코드 잘못올린거 수정

--- 이예서/25.04.16/2156 포도주 시식.js
+++ 이예서/25.04.16/2156 포도주 시식.js
@@ -4,19 +4,22 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-let arr = input[0];
-let bomb = input[1];
-let bomLen = bomb.length;
-let stack = [];
-let top = 0;
+const n = +input.shift();
+const wines = input.map(Number);
 
-for (let i = 0; i < arr.length; i++) {
-  stack.push(arr[i]);
-  top = arr[i];
-  if (top === bomb[bomLen - 1]) {
-    let last = stack.slice(-bomLen);
-    if (last.join("") === bomb) stack.splice(-bomLen);
+wines.unshift(0);
+
+function solution(n, wines) {
+  const dp = Array.from({ length: n + 1 }, () => 0);
+  dp[1] = wines[1];
+  dp[2] = wines[1] + wines[2];
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = Math.max(dp[i - 2], dp[i - 3] + wines[i - 1]) + wines[i];
+    dp[i] = Math.max(dp[i - 1], dp[i]);
   }
+
+  console.log(dp[n]);
 }
 
-console.log(stack.length ? stack.join("") : "FRULA");
+solution(n, wines);
